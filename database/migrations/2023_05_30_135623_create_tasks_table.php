a55c9d6d7989f9d6affb701d9a80a8923bb8036e
kota-tenma <133065682kota-tenma@users.noreply.github.com>
2023-05-30 14:05:24 +0900

    change database

--- database/migrations/2023_05_30_135623_create_tasks_table.php
+++ database/migrations/2023_05_30_135623_create_tasks_table.php
@@ -1,16 +1,16 @@
 <?php
-
+ 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+ 
 class CreateTasksTable extends Migration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+        * Run the migrations.
+        *
+        * @return void
+        */
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
@@ -20,12 +20,12 @@ class CreateTasksTable extends Migration
             $table->timestamps();
         });
     }
-
+ 
     /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+        * Reverse the migrations.
+        *
+        * @return void
+        */
     public function down()
     {
         Schema::dropIfExists('tasks');
